b8932c53b154c025eb058ac061fb9512496d6ded
Vy Hong <shiweistg@gmail.com>
2025-01-07 20:12:29 +0000

    Remove _fetchWebpage from importer.py

--- ir/importer/importer.py
+++ ir/importer/importer.py
@@ -50,8 +50,6 @@ from aqt.utils import (
     showWarning,
     tooltip,
 )
-from bs4 import BeautifulSoup
-from requests import get
 from requests.exceptions import ConnectionError
 
 from ir.lib.feedparser import parse
@@ -338,29 +336,6 @@ class Importer:
             html = f.read()
             url = urlunsplit(("file", "", filepath, None, None))
             return self._cleanWebpage(html, url, True)
-
-    def _fetchWebpage(self, url: str) -> BeautifulSoup:
-        if urlsplit(url).scheme not in ["http", "https"]:
-            raise ImporterError(
-                ErrorLevel.CRITICAL, "Only HTTP requests are supported."
-            )
-
-        try:
-            html = get(
-                url, headers={"User-Agent": self._settings["userAgent"]}, timeout=5
-            ).content
-            webpage = self._cleanWebpage(html, url)
-        except HTTPError as error:
-            raise ImporterError(
-                ErrorLevel.WARNING,
-                f"The remote server has returned an error: HTTP Error {error.code} ({error.reason})",
-            ) from error
-        except ConnectionError as error:
-            raise ImporterError(
-                ErrorLevel.WARNING, "There was a problem connecting to the website."
-            ) from error
-
-        return webpage
 
     def _createNote(self, title, text, source, priority=None):
         if self._settings["importDeck"]:
